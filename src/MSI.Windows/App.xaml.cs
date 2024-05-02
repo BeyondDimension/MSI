fdda89c949e37775ad6e9e71ebf64d90cc46eaeb
Aigio Liu <lysoft.pw@foxmail.com>
2024-05-02 11:55:21 +0800

    🚧 初始化项目

---
+++ src/MSI.Windows/App.xaml.cs
@@ -0,0 +1,8 @@
+namespace MSI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public sealed partial class App : Application
+{
+}
---
+++ src/MSI.Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+namespace MSI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
